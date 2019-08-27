4d71feef1d6b664b45a7279a9d32a0a0b3c2f9eb
Ahipeau <salimonu07@gmail.com>
2019-08-27 01:01:33 +0000

    Added files config, login and register

---
+++ inc/config.php
@@ -0,0 +1,11 @@
+ <?php
+
+//If there is the contant defined called _CONFIG_
+if (!defined('__CONFIG__')) {
+	# code...
+	exit ('You do not have a config file');
+}
+
+//
+
+ ?>
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+ <?php
+ //Allow the config
+define('__CONFIG__', true);
+
+//require config file
+require_once "inc/config.php";
+
+ ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -8,44 +16,35 @@
 
     <title>Page Title</title>
 
-    <base href="/" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+    <!-- <base href="/" /> -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.7/css/uikit.min.css" />
+
   </head>
 
   <body>
 
   	<div class="uk-section uk-container">
-  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-			<form class="uk-form-stacked js-login">
 
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Email</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
-			        </div>
-			    </div>
+  		<?php 
+  		echo "Hello world. Today is ";
+  		echo date("d m Y");
 
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Password</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
-			        </div>
-			    </div>
+  		?>
+  		<p>
+  		<a href="login.php">Login</a>
+  		<a href="register.php">Register</a>
 
-			    <div class="uk-margin">
-			        <button class="uk-button uk-button-default" type="submit">Login</button>
-			    </div>
+  		</p>
+  		
 
-			</form>
-  		</div>
+ 
+ 		
+
   	</div>
 
-    <!-- jQuery is required -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  	<?php require_once "inc/footer.php"; ?>
 
-    <!-- UIkit JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
+
 
   </body>
 </html>
